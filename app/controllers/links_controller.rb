ab1aa6918bf7e6d6dbc16e905c8311d11f05de53
David Stinnette <dastinnette@gmail.com>
2016-04-18 12:40:03 -0600

    Links successfully submitting to index page

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,11 +2,13 @@ class LinksController < ApplicationController
 
   def index
     @link = Link.new
+    @links = current_user.links
   end
 
   def create
     link = Link.new(link_params)
     if link.save
+      current_user.links << link
       flash[:notice] = "Link was created."
     else
       flash[:errors] = "Invalid. Try again."
