cd693516d7a1d67f9b4ff30fea5988e7ae2958c9
David Stinnette <dastinnette@gmail.com>
2016-04-18 14:27:40 -0600

    Fixed user log in test - full test suite passing

--- spec/features/user_logs_in_spec.rb
+++ spec/features/user_logs_in_spec.rb
@@ -3,13 +3,14 @@ require "rails_helper"
 RSpec.feature "user log in" do
 
   scenario "with correct credentials" do
+    create_user
     visit root_path
 
     click_on "log in"
 
     expect(current_path).to eq(login_path)
 
-    fill_in "Email", with: "david@example.com"
+    fill_in "Email", with: "test@example.com"
     fill_in "Password", with: "password"
     click_on "enter"
 
