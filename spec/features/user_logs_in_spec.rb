363a2ba2a8b106011390ba19ef5aaec2926e0b89
David Stinnette <dastinnette@gmail.com>
2016-04-18 13:45:32 -0600

    Sign up or log in tests fixed

--- spec/features/user_logs_in_spec.rb
+++ spec/features/user_logs_in_spec.rb
@@ -2,16 +2,12 @@ require "rails_helper"
 
 RSpec.feature "user log in" do
 
-  it "logs in with correct credentials" do
+  scenario "with correct credentials" do
     User.create(email: "david@example.com", password: "password")
     visit root_path
 
-    expect(page.status_code).to eq(200)
-    expect(page).to have_content("please log in or sign up")
+    click_on "log in"
 
-    click_link("log in")
-
-    expect(page).to have_content("log in")
     expect(current_path).to eq(login_path)
 
     fill_in "Email", with: "david@example.com"
--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -2,21 +2,17 @@ require "rails_helper"
 
 RSpec.feature "user sign in" do
 
-  it "signs up with correct credentials" do
+  scenario "with correct credentials" do
     visit root_path
 
-    expect(page.status_code).to eq(200)
-    expect(page).to have_content("please log in or sign up")
+    click_on "sign up"
 
-    click_link("sign up")
-
-    expect(page).to have_content("create a new account")
     expect(current_path).to eq(new_user_path)
 
-    fill_in "Email", with: "david@example.com"
+    fill_in "Email", with: "hello@email.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
-    click_on "create account"
+    click_button "create account"
 
     expect(current_path).to eq(links_path)
   end
