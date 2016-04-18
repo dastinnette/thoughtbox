64f9498311f91b712bcf5f61637d71b981a5ebe2
David Stinnette <dastinnette@gmail.com>
2016-04-18 12:48:10 -0600

    Added default read value to false for each link - showing in view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418182546) do
+ActiveRecord::Schema.define(version: 20160418184637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,10 +19,10 @@ ActiveRecord::Schema.define(version: 20160418182546) do
   create_table "links", force: :cascade do |t|
     t.string   "url"
     t.string   "title"
-    t.boolean  "read"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.integer  "user_id"
+    t.boolean  "read",       default: false
   end
 
   create_table "users", force: :cascade do |t|
