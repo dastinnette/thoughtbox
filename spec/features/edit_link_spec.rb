85be9add5feba7dccd59bdc1e3d1fa59c35f8be2
David Stinnette <dastinnette@gmail.com>
2016-04-18 15:27:34 -0600

    Successfully testing edit link sad path with bad url

--- spec/features/edit_link_spec.rb
+++ spec/features/edit_link_spec.rb
@@ -23,4 +23,19 @@ RSpec.feature "edit links" do
     expect(page).to_not have_content(link.url)
   end
 
+  scenario "invalid url not saved during edit" do
+    login_user
+    link = create_link
+    visit links_path
+
+    click_on "edit"
+
+    expect(current_path).to eq(edit_link_path(link))
+
+    fill_in "link_title", with: "More sports!"
+    fill_in "link_url", with: "esspnn.io"
+    click_on "update link"
+
+    expect(current_path).to eq(link_path(link))
+  end
 end
