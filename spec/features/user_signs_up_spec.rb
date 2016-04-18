178a696ec398650452dacf5ffd604b314f6f4918
David Stinnette <dastinnette@gmail.com>
2016-04-18 14:32:07 -0600

    User cannot create account without matching passwords - Iteration 1 fully complete/tested

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -9,12 +9,27 @@ RSpec.feature "user sign in" do
 
     expect(current_path).to eq(new_user_path)
 
-    fill_in "Email", with: "hello@email.com"
+    fill_in "Email", with: "test@example.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
     click_button "create account"
 
     expect(current_path).to eq(links_path)
+  end
+
+  scenario "rejected with non matching passwords" do
+    visit root_path
+
+    click_on "sign up"
+
+    expect(current_path).to eq(new_user_path)
+
+    fill_in "Email", with: "test@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "pw"
+    click_button "create account"
+
+    expect(current_path).to eq(new_user_path)
   end
 
 end
