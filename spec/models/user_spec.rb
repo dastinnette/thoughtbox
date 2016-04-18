6e61712ca931b39d334460ca9dc46ffa0591339b
David Stinnette <dastinnette@gmail.com>
2016-04-18 14:21:24 -0600

    Successfully testing email uniqueness in user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "must have a unique email address" do
+    user1 = User.create(email: "1@example.com", password: "password")
+    user2 = User.new(email: "1@example.com", password: "password")
+
+    expect(user2.save).to be false
+  end
+
 end
