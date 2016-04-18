4af16137ece071792e3851dfb120b7bac731749c
David Stinnette <dastinnette@gmail.com>
2016-04-18 12:20:57 -0600

    Added create for links and routes

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,4 +4,20 @@ class LinksController < ApplicationController
     @link = Link.new
   end
 
+  def create
+    link = Link.new(link_params)
+    if link.save
+      flash[:notice] = "Link was created."
+    else
+      flash[:errors] = "Invalid. Try again."
+    end
+    redirect_to links_path
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:title, :url)
+  end
+
 end
